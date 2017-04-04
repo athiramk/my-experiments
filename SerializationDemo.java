69bc17b382842ae9110c4988c3c1211a10377473
athiramk <mkathiramk@gmail.com>
2017-04-04 12:26:39 +0530

    Create SerializationDemo.java
    This is a sample java program which shows serialization and de-serialization techniques

---
+++ SerializationDemo.java
@@ -0,0 +1,48 @@
+/**
+ * This is a sample program which shows serialization and de-serialization
+ */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializationDemo implements Serializable {
+	int length;
+	int breadth;
+	
+	public SerializationDemo(int length, int breadth) {
+		this.length = length;
+		this.breadth = breadth;
+	}
+	public static void main(String[] args) throws Exception {
+		SerializationDemo sd = new SerializationDemo(10,12);
+		
+		FileOutputStream fileout = new FileOutputStream("sample.txt");
+		ObjectOutputStream objectout = new ObjectOutputStream(fileout);
+		
+		objectout.writeObject(sd);
+		
+		objectout.close();
+		fileout.close();
+		
+		System.out.println("Object has been serialized");
+
+
+		FileInputStream filein = new FileInputStream("sample.txt");
+		ObjectInputStream objectin = new ObjectInputStream(filein);
+		
+		SerializationDemo sd2 = null;
+		
+		sd2 = (SerializationDemo)objectin.readObject();
+		
+		objectin.close();
+		filein.close();
+		
+		System.out.println("Object has been deserialized ");
+        System.out.println("length = " + sd2.length);
+        System.out.println("breadth = " + sd2.breadth);
+		
+	}
+
+}
